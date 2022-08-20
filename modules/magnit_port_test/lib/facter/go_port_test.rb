6fded190982d44819f59663795c7b62b010fc5d1
gi.mr@yandex.ru <>
2022-08-21 00:43:59 +0300

    Переделка Puppet facter

--- modules/magnit_port_test/lib/facter/go_port_test.rb
+++ modules/magnit_port_test/lib/facter/go_port_test.rb
@@ -1,10 +1,17 @@
 Facter.add(:interfaces_array) do
   setcode do
-   interfaces = Facter.value(:interfaces)
+   interfaces = Facter::Core::Execution.execute("/home/gia/port_cheack_tcp magnit.ru 443")
    # the 'interfaces' fact returns a single comma-delimited string, such as "lo0,eth0,eth1"
    # this splits the value into an array of interface names
-   interfaces.split(',')
+   #interfaces.split(',')
   end
 end
 
 #test puppet
+
+
+
+
+
+
+
