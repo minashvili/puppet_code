536b05f923afa1a48c92cf363fa671b05bbd9af3
gi.mr@yandex.ru <>
2022-08-21 00:47:15 +0300

    Переделка Puppet facter

--- modules/magnit_port_test/lib/facter/go_port_test.rb
+++ modules/magnit_port_test/lib/facter/go_port_test.rb
@@ -1,14 +1,34 @@
-Facter.add(:interfaces_array) do
+Facter.add(:interfaces_hash) do
   setcode do
-   interfaces = Facter::Core::Execution.execute("/home/gia/port_cheack_tcp magnit.ru 443")
-   # the 'interfaces' fact returns a single comma-delimited string, such as "lo0,eth0,eth1"
-   # this splits the value into an array of interface names
-   #interfaces.split(',')
+    interfaces_hash = {}
+
+    Facter.value(:interfaces_array).each do |interface|
+      ipaddress = Facter.value("ipaddress_#{interface}")
+      if ipaddress
+        interfaces_hash[interface] = ipaddress
+      end
+    end
+
+    interfaces_hash
   end
 end
 
-#test puppet
 
+
+
+
+
+# Facter.add(:interfaces_array) do
+#   setcode do
+#    interfaces = Facter::Core::Execution.execute("/home/gia/port_cheack_tcp magnit.ru 443")
+#    # the 'interfaces' fact returns a single comma-delimited string, such as "lo0,eth0,eth1"
+#    # this splits the value into an array of interface names
+#    #interfaces.split(',')
+#   end
+# end
+#
+# #test puppet
+#
 
 
 
