73dce59ff90944997be8973e449b930f3762b356
gi.mr@yandex.ru <>
2022-08-21 01:10:31 +0300

    Ruby придумывания получение словаря с сервера, куда есть сетевой доступ по 443

--- modules/magnit_port_test/lib/facter/go_port_test.rb
+++ modules/magnit_port_test/lib/facter/go_port_test.rb
@@ -1,12 +1,13 @@
 Facter.add(:zabbix_true_server) do
   setcode do
-#     interfaces_hash = {}
     zabbix_true_server = {}
     zabbix_proxy_list = ['178.248.238.722', 'puppet.com', '178.248.238.723']
 
     zabbix_proxy_list.each do | x_port |
         fact_bule = Facter::Core::Execution.execute("/home/gia/port_cheack_tcp #{x_port} 443")
-        zabbix_true_server[x_port] = fact_bule
+        if fact_bule == 'true'
+          zabbix_true_server[x_port] = fact_bule
+        end
     end
 
     zabbix_true_server
@@ -14,9 +15,8 @@ Facter.add(:zabbix_true_server) do
 end
 
 
-#
-#
-#
+
+
 # Facter.add(:interfaces_hash) do
 #   setcode do
 #     interfaces_hash = {}
