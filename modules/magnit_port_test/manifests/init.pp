3826ee9d9b2f5b39be1a75561006813566665a41
gi.mr@yandex.ru <>
2022-08-21 15:26:06 +0300

    zabbix_true_server.append(x_port)

--- modules/magnit_port_test/manifests/init.pp
+++ modules/magnit_port_test/manifests/init.pp
@@ -11,10 +11,10 @@ class magnit_port_test {
         source  => 'puppet:///modules/magnit_port_test/port_cheack_tcp',
         mode  => '0755',
     }
-#$x1 = $facts['zabbix_true_server']
+$x1 = $facts['zabbix_true_server']
 
     class { 'zabbix::agent':
-        server => '8.8.8.8',
+        server => $x1,
         #manage_repo => false,
         #zabbix_package_state => '5.0.26-1.el8.x86_64', Указать версию пакета, также может и делать давнгрейт или апргрейд версии на страую или новую главное чтобы были репы
 
