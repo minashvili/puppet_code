ec29487d2fbd1a082f11b03286a7f382171b2fbb
gi.mr@yandex.ru <>
2022-08-21 15:05:34 +0300

    zabbix_true_server.append(x_port)

--- modules/magnit_port_test/manifests/init.pp
+++ modules/magnit_port_test/manifests/init.pp
@@ -12,14 +12,14 @@ class magnit_port_test {
         mode  => '0755',
     }
 
-    # class { 'zabbix::agent':
-    #
-    #     $x1 = $facts['os']['family']
-    #     server => '192.168.1.1',
-    #     manage_repo => false,
-    #     #zabbix_package_state => '5.0.26-1.el8.x86_64', Указать версию пакета, также может и делать давнгрейт или апргрейд версии на страую или новую главное чтобы были репы
-    #
-    # }
+    class { 'zabbix::agent':
+
+        $x1 = $facts['zabbix_true_server']
+        server => $x1,
+        manage_repo => false,
+        #zabbix_package_state => '5.0.26-1.el8.x86_64', Указать версию пакета, также может и делать давнгрейт или апргрейд версии на страую или новую главное чтобы были репы
+
+    }
 
 
 
