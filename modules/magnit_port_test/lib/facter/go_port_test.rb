d493654fa44d093f6bea1d86b90f6f24b93857be
gi.mr@yandex.ru <>
2022-08-21 17:24:55 +0300

    zabbix_true_server.append(x_port)

--- modules/magnit_port_test/lib/facter/go_port_test.rb
+++ modules/magnit_port_test/lib/facter/go_port_test.rb
@@ -3,7 +3,7 @@ Facter.add(:zabbix_true_server) do
   setcode do
     #zabbix_true_server = {}
     zabbix_true_server = []
-    zabbix_proxy_list = ['forge.puppet.com', 'puppet.com', '178.248.238.723']
+    zabbix_proxy_list = ['forge.puppet.comn', 'puppet.com', '178.248.238.723']
 
     zabbix_proxy_list.each do | x_port |
         fact_bule = Facter::Core::Execution.execute("/home/gia/port_cheack_tcp #{x_port} 443")
