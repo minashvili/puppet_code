5a97d5e16abf5db51c2757bd429d1a041d8b7dbb
gi.mr@yandex.ru <>
2022-08-23 21:02:03 +0300

    +

--- modules/magnit_port_test/lib/facter/go_port_test.rb
+++ modules/magnit_port_test/lib/facter/go_port_test.rb
@@ -1,28 +1,29 @@
 #Скрипт на Ruby пуляет скрипт на Golang чтобы получить список доступных хостов для DSL Puppet модуля установка Zabbix
-Facter.add(:zabbix_true_server) do
-  setcode do
-    #zabbix_true_server = {}
-    zabbix_true_server = []
-    zabbix_proxy_list = ['forge.puppet.comn', 'puppet.com', '178.248.238.723']
+golang_super_fact = "/etc/facter/port_cheack_tcp"
 
-    zabbix_proxy_list.each do | x_port |
-        fact_bule = Facter::Core::Execution.execute("/home/gia/port_cheack_tcp #{x_port} 443")
-        if fact_bule == 'true'
-          #zabbix_true_server[x_port] = fact_bule
-          zabbix_true_server.append(x_port)
-        end
+if File.exist?(golang_super_fact)
+  Facter.add(:zabbix_true_server) do
+    setcode do
+      #zabbix_true_server = {}
+      zabbix_true_server = []
+      zabbix_proxy_list = ['forge.puppet.comn', 'puppet.com', '178.248.238.723']
+
+      zabbix_proxy_list.each do | x_port |
+          fact_bule = Facter::Core::Execution.execute("/etc/facter/port_cheack_tcp #{x_port} 443")
+          if fact_bule == 'true'
+            #zabbix_true_server[x_port] = fact_bule
+            zabbix_true_server.append(x_port)
+          end
+      end
+
+      zabbix_true_server
     end
-
-    zabbix_true_server
   end
 end
 
-
-#Читать
+##Читать
 #https://puppet.com/docs/puppet/6/fact_overview.html#writing_facts_aggregate_resolutions-build-flat-fact
 #https://puppet.com/docs/puppet/6/custom_facts.html#aggregate_resolutions
-
-
 
 
 
