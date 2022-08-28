ff44197ea7d6510a776ef24ce09c4ccb9e6a734f
gi.mr@yandex.ru <>
2022-08-28 19:00:32 +0300

    Работа по настройка pip через puppet и установка django, ansible

--- modules/magnit_pip_install/manifests/init.pp
+++ modules/magnit_pip_install/manifests/init.pp
@@ -1,14 +1,27 @@
 class magnit_pip_install {
-  include pip
+#   include pip
+#
+#   pip::install { 'ansible':
+#   package        => 'ansible', # defaults to $title
+#   #version        => '1.6',    # if undef installs latest version
+#   #python_version => '3.3',    # defaults to 2.7
+#   ensure         => present,  # defaults to present
+#   #index_url      => ''
+# #qws
+#   }
 
-  pip::install { 'ansible':
-  package        => 'ansible', # defaults to $title
-  #version        => '1.6',    # if undef installs latest version
-  #python_version => '3.3',    # defaults to 2.7
-  ensure         => present,  # defaults to present
-  #index_url      => ''
-#qws
-  }
+   #31  yum install python3
+   #32  pip3 install --upgrade pip
+   #33  pip3 install ansible
+
+  #Установить python3 установить пип3
+  #
+
+
+
+
+
+
 
 
 }
