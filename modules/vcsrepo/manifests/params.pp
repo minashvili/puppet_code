5a9758a90a54215b472463d3d81861bc4579a77d
minashvili <gi.mr@yandex.ru>
2023-09-27 19:10:17 +0300

    тестирую манифест попытка №11.2

--- modules/vcsrepo/manifests/params.pp
+++ modules/vcsrepo/manifests/params.pp
@@ -3,7 +3,7 @@ class vcsrepo::params {
   $p_local_path                             = '/opt/george_test'
   $p_csv_path_repo                          = 'https://github.com/minashvili/prometheus_grafana.git'
   $p_branch_csv                             = 'master'
-  $p_revision                               = 'undef'
+  $p_revision                               = undef
 }
 
 
