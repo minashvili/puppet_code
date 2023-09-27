5d88eefff4843dde9507e88b9ac8091e6e0843fe
minashvili <gi.mr@yandex.ru>
2023-09-27 16:23:39 +0300

    тестирую манифест попытка №7.5

--- modules/vcsrepo/manifests/init.pp
+++ modules/vcsrepo/manifests/init.pp
@@ -3,7 +3,7 @@ class vcsrepo (
   $local_path                              = '/home/george',
   $csv_path_repo                           = 'git://github.com/openxpki/openxpki-docker.git',
   $branch_vcs                              = 'master',
-) inherits vcsrepo::params {
+) {
 
   Class['vcsrepo::manage::git'] -> Vcsrepo[$local_path]
 
