ebe7595560fe11156097dfeaf641fb5f261ea52c
minashvili <gi.mr@yandex.ru>
2023-09-27 17:51:27 +0300

    тестирую манифест попытка №10.1

--- modules/vcsrepo/manifests/init.pp
+++ modules/vcsrepo/manifests/init.pp
@@ -3,14 +3,16 @@ class vcsrepo (
   $local_path                              = $p_local_path,
   $csv_path_repo                           = $p_csv_path_repo,
   $branch_csv                              = $p_branch_csv,
+  $revision_hash                            = $p_revision,
 ) inherits vcsrepo::params {
 
 vcsrepo { $local_path:
   ensure   => 'present',
   provider => 'git',
   source   => $csv_path_repo,
-  branch   => $branch_vcs,
-  depth    => 1,
+  branch   => $branch_csv,
+  revision => $revision_hash,
+  # depth    => 1,
 }
 
 
