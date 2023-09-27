b56d803a47ad0be005803ffde201580e52db2849
minashvili <gi.mr@yandex.ru>
2023-09-27 18:01:39 +0300

    тестирую манифест попытка №10.2

--- modules/vcsrepo/manifests/params.pp
+++ modules/vcsrepo/manifests/params.pp
@@ -3,7 +3,7 @@ class vcsrepo::params {
   $p_local_path                             = '/opt/openxpki2'
   $p_csv_path_repo                          = 'https://github.com/openxpki/openxpki-docker.git'
   $p_branch_csv                             = 'master'
-  $p_revision                               = '7dadcb90587ed5cc165bc00afb935082fd1eb1d6'
+  $p_revision                               = undef
 }
 
 
