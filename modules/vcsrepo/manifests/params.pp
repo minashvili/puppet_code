f416eaa9143b57ebd27a4e49b49997ba96526531
minashvili <gi.mr@yandex.ru>
2023-09-27 16:08:11 +0300

    тестирую манифест попытка №3

--- modules/vcsrepo/manifests/params.pp
+++ modules/vcsrepo/manifests/params.pp
@@ -1,8 +1,8 @@
 
 class vcsrepo::params {
-  String                                  $p_csv_path_repo                           = 'https://github.com/openxpki/openxpki-docker.git'
-  String                                  $local_path_opt                          = '/home/george/'
-  Optional[String]                        $branch_vcs                              = 'development'
+  String                                  $p_csv_path_repo                         = 'git://github.com/openxpki/openxpki-docker.git'
+  String                                  $local_path_opt                          = '/home/george'
+  Optional[String]                        $branch_vcs                              = 'production'
 }
 
 
