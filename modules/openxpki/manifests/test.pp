8429788ccac5057551577df6aeeb0d2100892876
minashvili <gi.mr@yandex.ru>
2023-09-27 22:11:40 +0300

    Использую pdk создания манифеста.

---
+++ modules/openxpki/manifests/test.pp
@@ -0,0 +1,14 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include openxpki::test
+class openxpki::test {
+
+}
+
+
+
+
+
