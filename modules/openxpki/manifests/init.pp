64f4aa460bd2be3b67e3061c3afa55ca2b0490a3
minashvili <minashvili>
2023-10-13 15:33:42 +0300

    fork with puppet if and ruby

--- modules/openxpki/manifests/init.pp
+++ modules/openxpki/manifests/init.pp
@@ -3,21 +3,25 @@ class openxpki (
   $local_path                              = '/opt/my_custom_filename.rb'
 ){
 
-  $today_date_str = Timestamp.new()
+  $date_from_puppet_server = Timestamp.new()
+  $date_from_puppet_agent = str2time($facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1], '%Y-%m-%dT%H:%M:%SZ')
 
-  notify { 'resource title':
-    message  => "У этой строки тип ${$today_date_str} ${type($today_date_str)}"
+
+  notify { 'resource title1':
+    message  => "У этой строки тип ${$date_from_puppet_server} ${type($date_from_puppet_server)}"
   }
 
-  #$date_from_puppet_server = str2time($target_date_str, '%Y-%m-%dT%H:%M:%SZ')
-
-  if $facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1] == '2023-11-10T11:10:52Z' {
-
-    notify { 'resource title2':
-      message  => "Работает ${$facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1]}"
-    }
-
+  notify { 'resource title2':
+    message  => "У этой строки тип ${$date_from_puppet_agent} ${type($date_from_puppet_agent)}"
   }
+
+  # if $facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1] == '2023-11-10T11:10:52Z' {
+  #
+  #   notify { 'resource title2':
+  #     message  => "Работает ${$facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1]}"
+  #   }
+  #
+  # }
 
   # openxpki { 'test':
   #   ensure => present,
