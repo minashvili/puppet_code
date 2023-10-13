06f9b9597882f919c229fbd3c556c24987409149
minashvili <minashvili>
2023-10-13 17:52:38 +0300

    fork with puppet if and ruby

--- modules/openxpki/manifests/init.pp
+++ modules/openxpki/manifests/init.pp
@@ -3,9 +3,9 @@ class openxpki (
   $local_path                              = '/opt/my_custom_filename.rb'
 ){
 
-  $date_from_puppet_server = Timestamp.new()
   #Использование собственной функции $date_from_puppet_agent = openxpki::date_changer($facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1])
-  $date_from_puppet_agent = Timestamp.new($facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1])
+  $date_from_puppet_server = Timestamp.new()
+  $date_from_puppet_agent  = Timestamp.new($facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1])
 
   notify { 'resource title1':
     message  => "На сервере время ${$date_from_puppet_server} тип данных ${type($date_from_puppet_server)}"
@@ -14,7 +14,7 @@ class openxpki (
   notify { 'resource title2':
     message  => "На агенте вермя  ${$date_from_puppet_agent} тип данных ${type($date_from_puppet_agent)}"
   }
-  #Первожу тип данных "Длительность даты"(не путать с дата) в тип данных "строка" а потом в тип данных "Число" да это так делается!
+  #Первожу тип данных "Длительность даты"(не путать с дата) в тип данных "строка" а потом в тип данных "Число" да это так делается!!
   $date_result = Integer(($date_from_puppet_agent - $date_from_puppet_server).strftime('%D'))
 
   notify { 'resource title3':
