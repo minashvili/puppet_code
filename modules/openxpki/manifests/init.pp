cd31477280ed799fdea631b7e1e2edb283082152
minashvili <minashvili>
2023-10-13 14:32:45 +0300

    add fact for get date ssl cert №5

--- modules/openxpki/manifests/init.pp
+++ modules/openxpki/manifests/init.pp
@@ -2,10 +2,15 @@
 class openxpki (
   $local_path                              = '/opt/my_custom_filename.rb'
 ){
-  openxpki { 'test':
-    ensure => present,
-    path   => $local_path, # Указание пользовательского имени файла
+
+  notify { 'resource title':
+    message  => "Работает #{$facts['ssl_pki']}"
   }
+
+  # openxpki { 'test':
+  #   ensure => present,
+  #   path   => $local_path, # Указание пользовательского имени файла
+  # }
 
 }
 
