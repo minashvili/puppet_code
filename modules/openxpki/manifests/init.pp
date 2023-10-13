e82aab6830e46fd642a33302b2645b6e40ab3db7
minashvili <minashvili>
2023-10-14 00:41:44 +0300

    add first logic

--- modules/openxpki/manifests/init.pp
+++ modules/openxpki/manifests/init.pp
@@ -2,11 +2,11 @@
 class openxpki (
   String     $local_path                                = '/opt/my_custom_filename.rb',
   Timestamp  $date_from_puppet_server                   = $openxpki::params::date_server,
-#  Timestamp  $date_from_puppet_agent                    = $openxpki::params::date_agent,
-#  Integer    $date_result_ssl                           = $openxpki::params::date_result,
   Optional[Boolean] $force_get_ssl                      = $openxpki::params::force_param,
   Optional[String] $manual_commone_name                 = $openxpki::params::commone_name,
   Optional[String] $manual_alt_names                    = $openxpki::params::alt_names,
+  #  Timestamp  $date_from_puppet_agent                    = $openxpki::params::date_agent,
+  #  Integer    $date_result_ssl                           = $openxpki::params::date_result,
 ) inherits openxpki::params {
 
 
@@ -16,11 +16,11 @@ class openxpki (
 
     if $force_get_ssl == true or $date_result_ssl < 20 {
       notify { 'resource title2':
-        message  => "Работает! ${$date_result_ssl}"
+        message  => "Работает! ${$date_result_ssl} Запускаем бекап файлов и запускаем ресурс выписки сертификатов"
       }
     } else {
       notify { 'resource title3':
-        message => "Не Работает! ${$date_result_ssl}"
+        message => "Не Работает! ${$date_result_ssl} Ничего не запускаем штатно отрабатываемся без изменений"
       }
     }
   }
