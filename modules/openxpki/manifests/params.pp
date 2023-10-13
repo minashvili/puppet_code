f4a1b87faac3d970d19a5da42d9d606dc46fc2e5
minashvili <minashvili>
2023-10-13 23:04:06 +0300

    add test if and change parameters

--- modules/openxpki/manifests/params.pp
+++ modules/openxpki/manifests/params.pp
@@ -3,7 +3,7 @@ class openxpki::params {
   $date_server              = Timestamp.new()
   $date_agent               = Timestamp.new($facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1])
   $date_result              = Integer(($date_agent - $date_server).strftime('%D'))
-  $force_param              = undef
+  $force_param              = false
   # Первожу тип данных "Длительность даты"(не путать с дата)
   # в тип данных "строка" а потом в тип данных "Число" да это так делается!!!
 
