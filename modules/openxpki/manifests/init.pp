407c5ce00cf76336fda3d82a0ecf88d01706c2ad
minashvili <minashvili>
2023-10-13 15:00:08 +0300

    add fact for get date ssl cert №8

--- modules/openxpki/manifests/init.pp
+++ modules/openxpki/manifests/init.pp
@@ -3,10 +3,14 @@ class openxpki (
   $local_path                              = '/opt/my_custom_filename.rb'
 ){
 
+  notify { 'resource title':
+    message  => "У этой строки тип ${$facts['ssl_pki']['test2.corp.magneto.com']['ssl_date']}"
+  }
+
   if $facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1] == '2023-11-11T13:12:38Z' {
 
     notify { 'resource title':
-      message  => "Работает ${['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1]}"
+      message  => "Работает ${$facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1]}"
     }
 
   } #else {
