98598c202d64d77ef54aa0a03a2de1c5718b8cae
minashvili <minashvili>
2023-10-13 21:31:10 +0300

    add params.pp and change parameters

--- modules/openxpki/manifests/init.pp
+++ modules/openxpki/manifests/init.pp
@@ -1,11 +1,10 @@
 
 class openxpki (
-  $local_path                              = '/opt/my_custom_filename.rb'
-){
-
-  #Использование собственной функции $date_from_puppet_agent = openxpki::date_changer($facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1])
-  $date_from_puppet_server = Timestamp.new()
-  $date_from_puppet_agent  = Timestamp.new($facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1])
+  $local_path                              = '/opt/my_custom_filename.rb',
+  $date_from_puppet_server                 = $openxpki::params::date_server,
+  $date_from_puppet_agent                  = $openxpki::params::date_agent,
+  $date_result_ssl                         = $openxpki::params::date_result
+) inherits openxpki::params {
 
   notify { 'resource title1':
     message  => "На сервере время ${$date_from_puppet_server} тип данных ${type($date_from_puppet_server)}"
@@ -14,12 +13,11 @@ class openxpki (
   notify { 'resource title2':
     message  => "На агенте вермя  ${$date_from_puppet_agent} тип данных ${type($date_from_puppet_agent)}"
   }
-  #Первожу тип данных "Длительность даты"(не путать с дата) в тип данных "строка" а потом в тип данных "Число" да это так делается!!!
-  $date_result = Integer(($date_from_puppet_agent - $date_from_puppet_server).strftime('%D'))
 
   notify { 'resource title3':
     message  => "Разница в днях ${$date_result} тип данных ${type($date_result)}"
   }
+
   # if $facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1] == '2023-11-10T11:10:52Z' {
   #
   #   notify { 'resource title2':
---
+++ modules/openxpki/manifests/params.pp
@@ -0,0 +1,13 @@
+
+class openxpki::params {
+  $date_server              = Timestamp.new()
+  $date_agent               = Timestamp.new($facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1])
+  $date_result              = Integer(($date_agent - $date_server).strftime('%D'))
+  # Первожу тип данных "Длительность даты"(не путать с дата)
+  # в тип данных "строка" а потом в тип данных "Число" да это так делается!!!
+
+  #Использование собственной функции
+  #$date_from_puppet_agent = openxpki::date_changer($facts['ssl_pki']['test2.corp.magneto.com']['ssl_date'][1])
+
+}
+
